1c291e874358995efb3058eb6eaa12ad54607ce2
Ansel Adams <dj@volaciousbrands.com>
2018-05-21 21:35:38 -0500

    adding Word-Guess-Game

---
+++ Word-Guess-Game/assets/javascript/game.js
@@ -0,0 +1,11 @@
+//This is declaring two variables, guesses and score - score starts at 0, guesses starts at 10
+var score = 0;
+var guesses = 10;
+
+//This is setting an array of computerWordChoices
+var computerWordChoices = ["Harry", "Voldemort", "Hermione", "Ron", "Dumbledore"];
+
+if(i=0; i<computerWordChoices.length; i++) {
+    console.log(computerWordChoices[i])
+}
+
