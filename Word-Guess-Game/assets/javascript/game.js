e3efb71e4507a9b712332d4f24d3f09d18e0378c
Ansel Adams <dj@volaciousbrands.com>
2018-05-22 21:04:02 -0500

    added images and random generation

--- Word-Guess-Game/assets/javascript/game.js
+++ Word-Guess-Game/assets/javascript/game.js
@@ -3,9 +3,33 @@ var score = 0;
 var guesses = 10;
 
 //This is setting an array of computerWordChoices
-var computerWordChoices = ["Harry", "Voldemort", "Hermione", "Ron", "Dumbledore"];
+var computerWordChoices = ["Harry", "Voldemort", "Hermione", "Ron", "Dumbledore", "Hagrid"];
 
-if(i=0; i<computerWordChoices.length; i++) {
-    console.log(computerWordChoices[i])
-}
+//This is the computer randomly selecting 1 item from the array
+var randComputerSelection = computerWordChoices[Math.floor(Math.random() * computerWordChoices.length)];
+
+//Log the random computer selection
+console.log(randComputerSelection);
+
+//This is a if/else to change the image based on which word the computer randomly generates
+$(document).ready(function() {
+    if(randComputerSelection == "Harry") {
+        $("#potterImage").attr("src", "assets/images/Harry-Potter-in-cupboard-Jim-Kay-RGB-636x800.jpg");
+    }
+    else if(randComputerSelection == "Voldemort") {
+        $("#potterImage").attr("src","assets/images/HP1_Voldemort.jpg");
+    }
+    else if(randComputerSelection == "Hermione") {
+        $("#potterImage").attr("src","assets/images/Hermione_-_Jim_Kay_1_.jpg");
+    }
+    else if(randComputerSelection == "Ron") {
+        $("#potterImage").attr("src","assets/images/Ron_-_Jim_Kay.jpg");
+    }
+    else if(randComputerSelection == "Dumbledore") {
+        $("#potterImage").attr("src","assets/images/Pottermore_CharacterPortraits_Dumbledore_Colour_05JH.jpg")
+    }
+    else if(randComputerSelection == "Hagrid") {
+        $("#potterImage").attr("src","assets/images/Hagrid_-_Jim_Kay_1_.jpg");
+    }
+});
 
