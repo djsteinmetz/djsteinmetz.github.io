145a6fd7070f3a5539f6cdb48c278f473dc8bc8e
Editor <editor@BigiMac.local>
2018-06-13 17:08:09 -0500

    removed styling, added data-attr upon search

--- GifTastic/assets/javascript/app.js
+++ GifTastic/assets/javascript/app.js
@@ -1,5 +1,5 @@
 // Initialize variables
-var potterBtnArray = ["Harry Potter", "Hermione Granger", "Ronald Weasley", "Rubeus Hagrid", "Albus Dumbledore"];
+var topics = ["Harry Potter", "Hermione Granger", "Ronald Weasley", "Rubeus Hagrid", "Albus Dumbledore"];
 var userInput;
 var btnDisplay = $("#potterBtnDisplay");
 var newPotterBtn = $("<button>");
@@ -9,11 +9,12 @@ var characterCount = 0;
 // Initial display of buttons
 // TODO: Why is this making the array all one button???
 function initialDisplay() {
-    for(var i=0; i<potterBtnArray.length; i++) {
-        newPotterBtn.append(potterBtnArray[i]);
+    for(var i=0; i<topics.length; i++) {
+        newPotterBtn.append(" " + topics[i]);
         newPotterBtn.attr("class", "btn btn-outline-dark btn-sm");
         btnDisplay.append(newPotterBtn);
         console.log(newPotterBtn);
+        characterCount++
     };
 };
 
@@ -25,13 +26,13 @@ $("#submitBtn").on("click", function(event) {
     // Initialize new Bootstrap button
     newPotterBtn = $("<button>");
     // TODO: This data attribute with characterCount number is not working
-    newPotterBtn.attr("data-character" + characterCount);
+    newPotterBtn.attr("data-character", characterCount);
     newPotterBtn.attr("class", "btn btn-outline-dark btn-sm");
     newPotterBtn.append(userInput);
     btnDisplay.append(newPotterBtn);
     $("#userCharacterInput").val("");
-    potterBtnArray.push(userInput);
-    console.log(potterBtnArray);
+    topics.push(userInput);
+    console.log(topics);
     characterCount++
 });
 
