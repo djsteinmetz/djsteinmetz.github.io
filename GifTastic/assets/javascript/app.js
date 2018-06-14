11fcb0f7ffe8f98f8b1dc494dfcf9f92ef297830
Ansel Adams <dj@volaciousbrands.com>
2018-06-13 22:58:49 -0500

    enable animated gif display

--- GifTastic/assets/javascript/app.js
+++ GifTastic/assets/javascript/app.js
@@ -8,8 +8,8 @@ var characterCount = 0;
 function newBtn(name, count) {
     var newDisplayBtn = $("<button>")
     newDisplayBtn.append("" + name)
-        .attr("class", "btn btn-outline-dark btn-sm gifBtn")
-        .attr("data-character", count)
+        .addClass("btn btn-outline-dark btn-sm gifBtn")
+        .attr("data-character", name)
         .attr("id", "item-" + count);
 
     btnDisplay.append(newDisplayBtn);
@@ -32,8 +32,8 @@ $("#submitBtn").on("click", function(event) {
     // newBtn(topics, characterCount)
     // Replace the below with a method
     newPotterBtn = $("<button>")
-        .attr("data-character", characterCount)
-        .attr("class", "btn btn-outline-dark btn-sm")
+        .attr("data-character", userInput)
+        .addClass("btn btn-outline-dark btn-sm gifBtn")
         .attr("id", "item-" + characterCount)
         .append(userInput);
 
@@ -47,36 +47,37 @@ $("#submitBtn").on("click", function(event) {
 // Set different class for submit buttons
 // Can you specify "data-character" button?
 $(document).on("click", ".gifBtn", function() {
+    $("#potterGifs").html("");
     var character = $(this).attr("data-character");
     console.log(character);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      character + "&api_key=dc6zaTOxFJmzC&limit=10";
+      character + "&api_key=dc6zaTOxFJmzC&limit=50";
 
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      .then(function(response) {
+    .then(function(response) {
         var results = response.data;
         console.log(results);
 
-        // for (var i = 0; i < results.length; i++) {
-        //   var gifDiv = $("<div class='item'>");
+        for (var i = 0; i < 10; i++) {
+          var gifDiv = $("<div class='item'>");
+          var rating = results[i].rating;
+            if(rating !== "r") {
+            var p = $("<p>").text("Rating: " + rating);
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+            $("#potterGifs").prepend(gifDiv);
+            };
+        };
 
-        //   var rating = results[i].rating;
+    });
+});
 
-        //   var p = $("<p>").text("Rating: " + rating);
-
-        //   var personImage = $("<img>");
-        //   personImage.attr("src", results[i].images.fixed_height.url);
-
-        //   gifDiv.prepend(p);
-        //   gifDiv.prepend(personImage);
-
-        //   $("#gifs-appear-here").prepend(gifDiv);
-        // }
-      });
-  });
+// Add the .on("click") here for animating the gis?
   
 $(document).ready(function() {
     initialDisplay(); 
