51be17ae762b969a96656898ddf67b4139d04f74
Ansel Adams <dj@volaciousbrands.com>
2018-06-25 23:25:55 -0500

    psedo-code Hogwarts Express Schedule

--- Hogwarts-Express-Schedule/assets/javascript/app.js
+++ Hogwarts-Express-Schedule/assets/javascript/app.js
@@ -1,57 +1,68 @@
-// Database call
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyApltuvOtyyXPBfPc1PoBRUY5RPnSIibww",
     authDomain: "hogwarts-express-schedule.firebaseapp.com",
-    databaseURL: "https://hogwarts-express-schedule.firebaseio.com",
+    databaseURL: "https://hogwarts-express-schedule.firebaseio.com/",
     projectId: "hogwarts-express-schedule",
     storageBucket: "hogwarts-express-schedule.appspot.com",
     messagingSenderId: "737166235287"
 };
 firebase.initializeApp(config);
+
 var database = firebase.database();
+console.log("hello");
+// function calculateMonthsWorked(startDay) {
+//     var monthsWorked = moment(moment(), 'months').diff(startDay, "months");
+//     // Ternary statement to filter out the NAN values.  They are expressions, they evaluate to a value.
+//     // Not an if-then statement, it is an evaluation.
+//     return isNaN(monthsWorked) ? 0 : monthsWorked;
+// }
 
-function calculateMonthsWorked(startDay) {
-    var monthsWorked = moment(moment(), 'months').diff(startDay, "months");
-    // Ternary statement to filter out the NAN values.  They are expressions, they evaluate to a value.
-    // Not an if-then statement, it is an evaluation.
-    return isNaN(monthsWorked) ? 0 : monthsWorked;
-}
+// function calculateTotalBilled(monthsWorked, employeeMonthlyRate) {
+//     var totalBilled = (monthsWorked * employeeMonthlyRate);
+//     return totalBilled;
+// }
 
-function calculateTotalBilled(monthsWorked, employeeMonthlyRate) {
-    var totalBilled = (monthsWorked * employeeMonthlyRate);
-    return totalBilled;
-}
-
-$("#add-train").on("click", function (event) {
+$(document).on("click", "#add-train", function (event) {
     event.preventDefault();
     console.log("hello")
     // Display the info to the HTML
-    employeeName = $("#train-number-input").val().trim();
-    employeeRole = $("#destination-input").val().trim();
-    employeeStartDate = $("#first-train-input").val().trim();
-    employeeMonthlyRate = $("#frequency-input").val().trim();
+    var trainNumber = $("#train-number-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var firstTrain = $("#first-train-input").val().trim();
+    firstTrainConverted = moment(firstTrain, "HH");
+    console.log(firstTrainConverted);
+    var trainFrequency = $("#frequency-input").val().trim();
     // Pushing new data to the database
     database.ref().push({
-        employeeName: employeeName,
-        employeeRole: employeeRole,
-        employeeStartDate: employeeStartDate,
-        employeeMonthlyRate: employeeMonthlyRate,
+        train_number: trainNumber,
+        train_destination: trainDestination,
+        first_train: firstTrain,
+        train_frequency: trainFrequency,
         dataAdded: firebase.database.ServerValue.TIMESTAMP
     });
 });
 
-// database.ref().on("child_added", function (snapshot) {
-//     var monthsWorked = calculateMonthsWorked(snapshot.val().employeeStartDate);
-//     console.log("about to append to the DOM")
+database.ref().on("child_added", function (snapshot) {
+    var newTrain = snapshot.val();
+    console.log(newTrain)
+    var newRow = $("<tr>");
+    var nameTd = $("<td>");
+    var destTd = $("<td>");
+    var arriveTd = $("<td>");
+    var freqTd = $("<td>");
+    nameTd.text(newTrain.train_number);
+    destTd.text(newTrain.train_destination);
+    arriveTd.text(newTrain.first_train);
+    freqTd.text(newTrain.train_frequency);
+    
+    newRow.append(nameTd);
+    newRow.append(destTd);
+    newRow.append(arriveTd);
+    newRow.append(freqTd);
+    $(".tableBody").append(newRow);
+    console.log("about to append to the DOM")
 
-//     var totalBilled = calculateTotalBilled(monthsWorked, snapshot.val().employeeMonthlyRate)
-//     console.log(totalBilled);
-//     // console.log(snapshot.val())
-//     $("#nameDisplay").append("<p>" + snapshot.val().employeeName + "</p>");
-//     $("#roleDisplay").append("<p>" + snapshot.val().employeeRole + "</p>");
-//     $("#startDateDisplay").append("<p>" + snapshot.val().employeeStartDate + "</p>");
-//     $("#monthlyRateDisplay").append("<p>" + snapshot.val().employeeMonthlyRate + "</p>");
-//     $("#monthsWorkedDisplay").append("<p>" + monthsWorked + " months" + "</p>");
-//     $("#totalBilledDisplay").append("<p>" + "$" + totalBilled);
-// });
+    // Here is where I would code in the logic to calculate the time until train arrival 
+    // as well as the time till the next trains.
+});
